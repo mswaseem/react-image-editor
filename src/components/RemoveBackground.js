1df4513b73772fddebccaec6a2303bf11e067e28
mswaseem <iamwaseem98@gmail.com>
2022-11-03 13:31:00 +0530

    deploy

--- src/components/RemoveBackground.js
+++ src/components/RemoveBackground.js
@@ -60,7 +60,6 @@ export default function RemoveBackground() {
       const image = document.getElementById("imageResult");
       const down = document.getElementById("down");
       image.src = blob;
-      down.href = "/#";
       down.download = "save.png";
     });
   };
