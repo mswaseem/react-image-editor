8f891faf7050770470035d2f219a5c3c4f1b6248
mswaseem <iamwaseem98@gmail.com>
2022-11-05 23:56:17 +0530

    code-one

--- src/components/RemoveBackground.js
+++ src/components/RemoveBackground.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setActionStatus } from "../features/removeBackground";
 import loadImage from "blueimp-load-image";
-import ColorFromImage from 'color-from-image'
+import ColorFromImage from "color-from-image";
+import * as htmlToImage from "html-to-image";
+import * as download from "downloadjs";
 
 export default function RemoveBackground() {
   const status = useSelector((state) => state.status.bgRemoved);
@@ -14,13 +16,20 @@ export default function RemoveBackground() {
   const [PreviewImage, setPreviewImage] = useState(null);
   const [colorCode, setColorCode] = useState(null);
 
+  useEffect(() => {
+    const image = document.getElementById("imageResult");
+    if (image) {
+      image.style.backgroundColor = colorCode;
+    }
+  }, [colorCode]);
+
   const imgUpload = (e) => {
     const img = e.target.files[0];
     let input = document.getElementById("upload");
     let infoArea = document.getElementById("upload-label");
     let fileName = input.files[0].name;
     infoArea.textContent = "File name: " + fileName;
-    setPreviewImage(URL.createObjectURL(img))
+    setPreviewImage(URL.createObjectURL(img));
 
     setImage(img);
     setColorCode(null);
@@ -43,7 +52,7 @@ export default function RemoveBackground() {
       const response = await fetch("https://api.remove.bg/v1.0/removebg", {
         method: "POST",
         headers: {
-          "X-Api-Key": "Rn1PbjhV5MbZvVahFJ7jfzoh",
+          "X-Api-Key": "WfaYr9xo8P2fTevdEvYqXvCH",
         },
         body: formData,
       });
@@ -57,12 +66,25 @@ export default function RemoveBackground() {
       const outputBlob = await response.blob();
 
       blob = URL.createObjectURL(outputBlob);
-      const image = document.getElementById("imageResult");
+      let image = document.getElementById("imageResult");
+      image.style.backgroundColor = colorCode;
       const down = document.getElementById("down");
       image.src = blob;
-      down.href = blob;
-      down.download = "save.png";
     });
+  };
+
+  const downloadConvertedImage = () => {
+    const node = document.getElementById("image-cont");
+    if (node) {
+      htmlToImage
+        .toPng(node)
+        .then(function (dataUrl) {
+          download(dataUrl, "my-image.png");
+        })
+        .catch(function (error) {
+          console.error("oops, something went wrong!", error);
+        });
+    }
   };
 
   return (
@@ -83,7 +105,6 @@ export default function RemoveBackground() {
           >
             Choose file
           </label>
-
           <div className="input-group-append">
             <label for="upload" className="btn btn-light m-0 rounded-pill px-4">
               {" "}
@@ -100,7 +121,8 @@ export default function RemoveBackground() {
                 setColorCode(null);
                 let infoArea = document.getElementById("upload-label");
                 infoArea.textContent = "Choose file";
-              }}>
+              }}
+            >
               {" "}
               <i className="fa fa-refresh mr-2 text-muted"></i>
               <small className="text-uppercase font-weight-bold text-muted">
@@ -109,80 +131,92 @@ export default function RemoveBackground() {
             </label>
           </div>
         </div>
-        {PreviewImage && <><div>
-          <header className="text-white text-center">
-            <h4 className="3">Please click on the image to select the color</h4>
-          </header>
-        </div>
-          <div className="row">
-            <div className="col">
-              <ColorFromImage
-                src={PreviewImage}
-                onChooseColor={(val) => setColorCode(val)}
-                hideColor={PreviewImage ? false : true}
-              />
+        {PreviewImage && (
+          <>
+            <div>
+              <header className="text-white text-center">
+                <h4 className="3">
+                  Please click on the image to select the color
+                </h4>
+              </header>
             </div>
-            <div className="col">
-              <div className="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
-                <input
-                  type="text"
-                  className="form-control border-0"
+            <div className="row">
+              <div className="col">
+                <ColorFromImage
+                  src={PreviewImage}
+                  onChooseColor={(val) => setColorCode(val)}
+                  hideColor={PreviewImage ? false : true}
                 />
-                <label
-                  id="upload-label"
-                  for="upload"
-                  className="font-weight-light text-muted"
-                >
-                  {colorCode ? colorCode : "Color"}
-                </label>
+              </div>
+              <div className="col">
+                <div className="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
+                  <input type="text" className="form-control border-0" />
+                  <label
+                    id="upload-label"
+                    for="upload"
+                    className="font-weight-light text-muted"
+                  >
+                    {colorCode ? colorCode : "Color"}
+                  </label>
+                </div>
               </div>
             </div>
-          </div></>}
-        {colorCode && <>
-          <button
-            className="btn btn-outline-light remove-button"
-            onClick={uploadImage}
-          >
-            Remove Background
-          </button>
-          <div>
-            <div className="row py-4">
-              <div className="col-lg-6 mx-auto text-center">
-                <p className="font-italic text-white text-center">
-                  The result will be rendered inside the box below.
-                </p>
-                <div className="image-area mt-4 justify-content-center">
-                  {status === false ? (
-                    <div class="lds-roller mb-3">
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                      <div></div>
-                    </div>
-                  ) : (
-                    <img
-                      id="imageResult"
-                      src="#"
-                      alt=""
-                      className="img-fluid rounded shadow-sm mx-auto d-block"
-                    />
-                  )}{" "}
-                </div>
-                {status ? (
-                  <a id="down">
-                    <button className="btn btn-light down-button">
+          </>
+        )}
+        {colorCode && (
+          <>
+            <button
+              className="btn btn-outline-light remove-button"
+              onClick={uploadImage}
+            >
+              Replace Background Color
+            </button>
+            <div>
+              <div className="row py-4">
+                <div className="col-lg-6 mx-auto text-center">
+                  <p className="font-italic text-white text-center">
+                    The result will be rendered inside the box below.
+                  </p>
+                  <div className="image-area mt-4 justify-content-center">
+                    {status === false ? (
+                      <div class="lds-roller mb-3">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    ) : (
+                      <>
+                        <div id="image-cont">
+                          <img
+                            id="imageResult"
+                            src="#"
+                            alt=""
+                            className="img-fluid rounded shadow-sm mx-auto d-block"
+                          />
+                        </div>
+                      </>
+                    )}{" "}
+                  </div>
+                  {status ? (
+                    <button
+                      className="btn btn-light down-button"
+                      onClick={downloadConvertedImage}
+                    >
                       {" "}
                       <i className="fas fa-download" /> Download
                     </button>
-                  </a>
-                ) : null}
+                  ) :
+                    null}
+                </div>
               </div>
             </div>
-          </div></>}
+          </>
+        )}
       </div>
     </div>
   );
